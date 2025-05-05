c93ddaf044e91226bda44de32e2f10d6b290c3c1
unknown <Enoch Morris>
2025-05-05 13:50:12 -0400

    Seemingly Completed Exercise 1 'HotelOperations' in Workbook 4

---
+++ HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        double basePrice;
+
+        if (roomType.equalsIgnoreCase("king")) {
+            basePrice = 139.00;
+        } else if (roomType.equalsIgnoreCase("double")) {
+            basePrice = 124.00;
+        } else {
+            throw new IllegalArgumentException("Invalid room type: " + roomType);
+        }
+
+        if (isWeekend) {
+            basePrice *= 1.10;
+        }
+        return basePrice;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setIsWeekend(boolean isWeekend) {
+        this.isWeekend = isWeekend;
+    }
+
+    public double getReservationTotal() {
+        return getPrice() * numberOfNights;
+    }
+}
